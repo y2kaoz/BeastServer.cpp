eb6486b0484971a4242a4fbff1abae79f7c03631
Carlos Gonzalez Y2KaoZ <y2kaoz@gmail.com>
2022-03-16 19:12:40 -0400

    Added basic options.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstdlib>
+#include <gsl/gsl_util>
 #include <memory>
 #include <thread>
 
@@ -64,41 +65,74 @@ private:
   }
 };
 
-auto main(int /*argc*/, char** /*argv*/) -> int {
+class Options {
+public:
+  static const int DEFAULT_PORT = 8080;
+
+  Options(int /*argc*/, char** /*argv*/) {
+  }
+  auto overrideConfig(int /*argc*/, char** /*argv*/) -> bool {
+    return true;
+  }
+  [[nodiscard]] auto cfgFile() const noexcept -> const std::string& {
+    return cfgFile_;
+  }
+  [[nodiscard]] auto docRoot() const -> std::filesystem::path {
+    return std::filesystem::canonical(docRoot_);
+  }
+  [[nodiscard]] auto threads() const -> std::size_t {
+    return threads_ > 0 ? threads_ : 1;
+  }
+  [[nodiscard]] auto endpoint() const -> boost::asio::ip::tcp::endpoint {
+    const auto address = boost::asio::ip::make_address(address_);
+    return boost::asio::ip::tcp::endpoint{address, gsl::narrow_cast<std::uint16_t>(port_)};
+  }
+
+private:
+  std::string cfgFile_{};
+  std::string address_{"0.0.0.0"};
+  int port_{DEFAULT_PORT};
+  std::string docRoot_{"."};
+  std::size_t threads_{std::thread::hardware_concurrency()};
+};
+
+auto main(int argc, char** argv) -> int {
   try {
-  const std::uint16_t PORT = 8080;
+    Options options(argc, argv);
+    if (!options.overrideConfig(argc, argv)) {
+      return EXIT_FAILURE;
+    }
 
-  std::size_t numThreads = std::thread::hardware_concurrency();
-  boost::asio::io_context ioContext{static_cast<int>(numThreads > 0 ? numThreads : 1)};
+    boost::asio::io_context ioContext{static_cast<int>(options.threads())};
 
-  boost::asio::signal_set signals(ioContext, SIGINT, SIGTERM);
-  signals.async_wait([&ioContext](auto, auto) { ioContext.stop(); });
+    boost::asio::signal_set signals(ioContext, SIGINT, SIGTERM);
+    signals.async_wait([&ioContext](auto, auto) { ioContext.stop(); });
 
-  auto endpoint = boost::asio::ip::tcp::endpoint{boost::asio::ip::make_address("0.0.0.0"), PORT};
+    auto endpoint = options.endpoint();
 
-      BOOST_LOG_TRIVIAL(info) << "This server is accepting connections to: '" << endpoint << "' with "
-                            << numThreads << " threads.";
+    BOOST_LOG_TRIVIAL(info) << "This server is accepting connections to: '" << endpoint << "' with "
+                            << options.threads() << " threads.";
 
-  std::make_shared<Y2KaoZ::Network::Tcp::Acceptor>(
-    ioContext,
-    endpoint,
-    std::make_shared<Y2KaoZ::Network::Tcp::Http::AcceptorHandler>(
-      ".",
-      std::make_shared<Y2KaoZ::Network::Tcp::Http::WebSocket::HttpSessionHandler>(
-        std::make_shared<ChatWebSocketSessionHandler>())))
-    ->accept();
+    std::make_shared<Y2KaoZ::Network::Tcp::Acceptor>(
+      ioContext,
+      endpoint,
+      std::make_shared<Y2KaoZ::Network::Tcp::Http::AcceptorHandler>(
+        options.docRoot(),
+        std::make_shared<Y2KaoZ::Network::Tcp::Http::WebSocket::HttpSessionHandler>(
+          std::make_shared<ChatWebSocketSessionHandler>())))
+      ->accept();
 
-  std::vector<std::thread> threads;
-  threads.reserve(numThreads - 1);
-  for (auto i = numThreads - 1; i > 0; --i) {
-    threads.emplace_back([&] { ioContext.run(); });
-  }
-  ioContext.run();
-  for (auto& thread : threads) {
-    thread.join();
-  }
-  BOOST_LOG_TRIVIAL(info) << "The server is all done.";
-  return EXIT_SUCCESS;
+    std::vector<std::thread> threads;
+    threads.reserve(options.threads() - 1);
+    for (auto i = options.threads() - 1; i > 0; --i) {
+      threads.emplace_back([&] { ioContext.run(); });
+    }
+    ioContext.run();
+    for (auto& thread : threads) {
+      thread.join();
+    }
+    BOOST_LOG_TRIVIAL(info) << "The server is all done.";
+    return EXIT_SUCCESS;
   } catch (const std::exception& e) {
     BOOST_LOG_TRIVIAL(fatal) << "Exception: " << e.what();
     return EXIT_FAILURE;
